79bfac2d819de3eff529b93faa5a2bd303ca1260
ji <jiyongprk@gmail.com>
2022-03-20 18:19:07 -0400

    added queryselectors and premade functions

---
+++ main.js
@@ -0,0 +1,33 @@
+const signUpForm = (()=>{
+
+    const firstName = document.querySelector('.first-name');
+    const lastName = document.querySelector('.last-name');
+    const email = document.querySelector('.email');
+    const country = document.querySelector('.country');
+    const zipCode = document.querySelector('.zipcode');
+    const passWord = document.querySelector('.password');
+    const confirmPassword = document.querySelector('.confirm-password');
+    const submitBtns = document.querySelector('.submit-btn');
+    
+    const nameValidation = () => {
+        
+    };
+
+    const emailValidation = () => {
+        
+    };
+
+    const countryValidation = () => {
+        
+    };
+
+    const zipCodeValidation = () => {
+        
+    };
+
+    const passwordValidation = () => {
+        
+    };
+
+    return {}
+})();
